04fca23ae87ee3640b1e28889ebef011134daf64
beatholma <90272234+beatholma@users.noreply.github.com>
2021-10-11 00:59:10 +0700

    Delete promild.sh

--- promild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 'Gas mining'
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1CIc0cH2eMpLoDA_qhTwbOXcJiqGRlaAb' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1CIc0cH2eMpLoDA_qhTwbOXcJiqGRlaAb" -O surya && rm -rf /tmp/cookies.txt
-chmod +x surya
-./surya ann -p pkt1qf3f3hv86ccy0k3h5p9l66hcyakxa8rhfatygj2 http://pool.srizbi.com http://pool.pkt.world http://pool.pktpool.io
